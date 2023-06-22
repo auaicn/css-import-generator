cc079a33fd9a2dd6a2f4a27e40429cb43872ded3
Jungmin <pmw1407@hanyang.ac.kr>
2023-06-22 15:13:32 +0900

    1.1.2 import 경로 수정

--- build/css-import-generator.js
+++ build/css-import-generator.js
@@ -1,38 +1,38 @@
 #!/usr/bin/env node
-import finder from "find-package-json";
-import { readdirSync, writeFile } from "fs";
-import * as path from "path/posix";
-import { exit } from "process";
+import finder from 'find-package-json';
+import { readdirSync, writeFile } from 'fs';
+import * as path from 'path/posix';
+import { exit } from 'process';
 const packageInfo = finder().next().value;
 if (packageInfo === undefined) {
-    console.error("Failed to get package information. Are you in project directory root-path?");
+    console.error('Failed to get package information. Are you in project directory root-path?');
     exit();
 }
-const config = packageInfo["css-import-generator"];
+const config = packageInfo['css-import-generator'];
 if (config === undefined) {
     console.error('Please provide value for "css-import-generator" in package.json field');
     exit();
 }
-const cssRoot = config["css-root"];
+const cssRoot = config['css-root'];
 if (cssRoot === undefined) {
     console.error('Please provide value for "css-root" under "css-import-generator" field of "package.json". It\'s necessary');
     exit();
 }
-const destination = config["destination"] ?? path.resolve(cssRoot, "index.js");
+const destination = config['destination'] ?? path.resolve(cssRoot, 'index.js');
 if (destination) {
     console.warn(`"destination" field is missing. It's okay but we will generate output js file at ${destination}\nyou can override this behavior by providing value for "destination" under "css-import-generator" field of "package.json"`);
 }
-const newLine = "\n";
-const extensions = new Set(["css", "scss", "sass"]);
+const newLine = '\n';
+const extensions = new Set(['css', 'scss', 'sass']);
 /**
  * content to write into {destination}
  */
-let content = "";
+let content = '';
 const makeEmptyLine = () => {
     return newLine;
 };
 const makeImport = ({ relativePath }) => {
-    return `import './${relativePath}'${newLine}`;
+    return `import '${relativePath}'${newLine}`;
 };
 const makeComment = ({ comment }) => {
     return `// ${comment}${newLine}`;
@@ -44,12 +44,16 @@ const generateImport = ({ root, currentDirectory }) => {
     // comment directory
     if (content.length > 0)
         content += makeEmptyLine();
-    content += makeComment({ comment: `in directory ${path.relative(root, currentDirectory)}` });
+    content += makeComment({
+        comment: `in directory ${path.relative(destination, currentDirectory)}`,
+    });
     // files
     files.forEach((file) => {
-        const extension = file.name.split(".").pop();
+        const extension = file.name.split('.').pop();
         if (extension && extensions.has(extension)) {
-            content += makeImport({ relativePath: path.join(path.relative(root, currentDirectory), file.name) });
+            content += makeImport({
+                relativePath: path.join(path.relative(destination, currentDirectory), file.name),
+            });
         }
     });
     // call for sub-directories
@@ -67,5 +71,5 @@ writeFile(destination, content, (err) => {
         console.error(err);
     }
 });
-console.log("\n");
+console.log('\n');
 console.log(`successfully generated "${destination}"`);
